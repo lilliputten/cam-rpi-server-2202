8d9dbbdf088dfc85073ee142b622c8a315992460
lilliputten <lilliputten@yandex.ru>
2022-02-07 23:59:59 +0300

    Scripts updated

--- utils/rpi-start-server.sh
+++ utils/rpi-start-server.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# @desc Update/restart server on local device server (rpi)
+# @module rpi-start-server.sh
+# @desc Update/restart server on local device server (rpi).
 # @since 2022.02.07, 22:01
-# @changed 2022.02.07, 23:31
+# @changed 2022.02.07, 23:59
+#
+# Script may be called from command line directly or under crontab. See crrsp sections in README.md file.
+#
 # @see https://peppe8o.com/beginners-guide-to-install-a-flask-python-web-server-on-raspberry-pi/
 # @see https://docs.gunicorn.org/en/stable/run.html
 # @see utils/rpi-gunicorn-help.txt
@@ -9,10 +13,13 @@
 # Full path to gunicorn python script (for running under crontab; use `command -v gunicorn`)
 GUNICORN="/home/pi/.local/bin/gunicorn"
 
-# Determine real project path...
+# Determine real project path (is required for crontab execution)...
 REALFILE=`realpath "$0"`
 REALDIR=`dirname "$REALFILE"`
 ROOT=`dirname "$REALDIR"`
+
+PIDFILE="$ROOT/.gunicorn.pid"
+LOGFILE="$ROOT/.gunicorn.log"
 
 # # Import config variables (expected variables `$DIST_REPO` and `$PUBLISH_FOLDER`)...
 # test -f "$REALDIR/utils/config.sh" && . "$REALDIR/utils/config.sh"
@@ -21,8 +28,8 @@ ROOT=`dirname "$REALDIR"`
 echo "Starting server in folder: $ROOT" # (via $REALFILE)"
 
 # Is running instance exists?
-if [ -f "$ROOT/.gunicorn.pid" ]; then
-  PID=`cat "$ROOT/.gunicorn.pid"`
+if [ -f "$PIDFILE" ]; then
+  PID=`cat "$PIDFILE"`
   echo "Found running instance with pid '$PID'. Trying to stop it."
   kill $PID
   # NOTE: You can use:
@@ -32,7 +39,8 @@ if [ -f "$ROOT/.gunicorn.pid" ]; then
 fi
 
 # Remove all log files...
-rm -f "$ROOT/log*.txt" "$ROOT/.gunicorn.*"
+rm -f "$ROOT/.gunicorn.*"
+# "$ROOT/log*.txt" # NOTE: Remove log files?
 
 # Start daemon...
 echo "Starting daemon..." \
@@ -42,9 +50,9 @@ echo "Starting daemon..." \
   -w 1 \
   -b 0.0.0.0:4000 \
   --chdir "$ROOT" \
-  --pid="$ROOT/.gunicorn.pid" \
-  --log-file="$ROOT/.gunicorn.log" \
+  --pid="$PIDFILE" \
+  --log-file="$LOGFILE" \
   index \
-&& echo OK
+&& echo "Successfully started with pid: `cat $PIDFILE`"
 
 # TODO: Extract port number (and other options?) to config?
