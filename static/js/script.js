9816ccbbb863b8164d72754883e9ef45773a6f17
lilliputten <lilliputten@yandex.ru>
2022-02-12 04:52:56 +0300

    Implemented simple socket.io tests. Relocated cdn files.

--- static/js/script.js
+++ static/js/script.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
     io: typeof io,
     socket: socket,
   });
-  // debugger;
   // this is a callback that triggers when the "message" event is emitted by the server.
   socket.on('message', function(data) {
     console.log('@:script:message', data);
