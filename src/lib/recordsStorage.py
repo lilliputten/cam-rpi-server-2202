e77e1e62a59a2e49b232a3d2f91ef9cf6e31980f
lilliputten <lilliputten@yandex.ru>
2022-02-15 03:10:18 +0300

    Restructured source tree: using `src/*` structure.

---
+++ src/lib/recordsStorage.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# @module recordsStorage
+# @desc Extendable (?) records storage engine
+# @since 2022.02.15, 01:38
+# @changed 2022.02.15, 01:38
+
+
+#  from .logger import DEBUG
+
+
+#  recordsData = []
+
+
+def addRecord(ownerId, data):
+    #  recordData = {
+    #      # timestamp
+    #      'ownerId': ownerId,
+    #      'data': data,
+    #  }
+    #  fromId = '@:recordsStorage:addRecord'
+    #  print(fromId, ownerId, data)
+    #  #  DEBUG(fromId, recordData)
+    #  recordsData.append(recordData)
+    return True
+
+
+#  __all__ = [  # Exporting objects...
+#      'addRecord',
+#  ]
+
+if __name__ == '__main__':
+    print('@:recordsData: debug run')
